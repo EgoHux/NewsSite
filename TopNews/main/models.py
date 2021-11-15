06a46c0517a0edc4c0092b79e95c1a4415bcc23b
ЖÐека <zenRbalahnine@gmail.com>
2021-11-15 22:23:04 +0700

    Добавил БД из 4-x сущностей

--- TopNews/main/models.py
+++ TopNews/main/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
+
+# python manage.py makemigrations
+# python manage.py migrate
+
+
 # Create your models here.
+class Roles(models.Model):
+    nameRole = models.CharField(max_length=20)
+
+
+# models.CASCADE: автоматически удаляет строку из зависимой таблицы, если удаляется связанная строка из главной таблицы
+
+# models.PROTECT: блокирует удаление строки из главной таблицы, если с ней связаны какие-либо строки из зависимой таблицы
+
+# models.SET_NULL: устанавливает NULL при удалении связанной строка из главной таблицы
+
+# models.SET_DEFAULT: устанавливает значение по умолчанию для внешнео ключа в зависимой таблице. В этом случае для этого столбца должно быть задано значение по умолчанию
+
+# models.DO_NOTHING: при удалении связанной строки из главной таблицы не производится никаких действий в зависимой таблице
+
+
+class Users(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    login = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    role = models.ForeignKey(Roles, null=True, on_delete=models.SET_NULL)
+
+
+class Posts(models.Model):
+    date = models.DateField()
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    image = models.ImageField()
+    viewers = models.IntegerField()
+
+
+class Likes(models.Model):
+    user = models.ForeignKey(Users, null=True, on_delete=models.SET_NULL)
+    post = models.ForeignKey(Posts, null=True, on_delete=models.SET_NULL)
+    rate = models.BooleanField(null=True)
+
+
+class Comments(models.Model):
+    user = models.ForeignKey(Users, null=True, on_delete=models.SET_NULL)
+    post = models.ForeignKey(Posts, null=True, on_delete=models.SET_NULL)
+    date = models.DateField()
+    text = models.TextField()
