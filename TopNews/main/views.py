9fc511520f32129c4cfe131df53ce0b90546857f
Dmitry Aratin <dima.aratin0@gmail.com>
2021-11-30 15:47:35 +0300

    Добавил счетчик просмотров/лайков/дизлайков и вывод информации о конкретной статье

--- TopNews/main/views.py
+++ TopNews/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . models import Posts
+from .models import Posts, Reporters, Likes, Users
 
 
 def index(request):
@@ -32,7 +32,19 @@ def top_news(request):
 
 def news_frame(request, id):
     post = Posts.objects.get(id=id)
-    return render(request, 'main/news_frame.html', {'title': 'title...', 'post': post})
+    post.viewers += 1
+    post.save()
+    reporter_obj = Reporters.objects.get(id=post.reporter.id)
+    reporter = (reporter_obj.site) if reporter_obj.site != None else reporter_obj.user
+    is_site = hasattr(reporter, 'base_url')
+
+    # Запихать сюда кол-во лайков/дизлайков и какой статус у авторизованного пользователя(bool)
+    likes = len(Likes.objects.filter(post = post.id, rate = True))
+    dislikes = len(Likes.objects.filter(post = post.id, rate = False))
+    user_is_rating = False # Получить ID пользователя и узнать какая оценка у авторизованного пользователя к этому посту
+    rating = {'likes': likes, 'dislikes': dislikes, 'user_is_rating': user_is_rating}
+
+    return render(request, 'main/news_frame.html', {'title': 'title...', 'post': post, 'reporter_info': {'reporter': reporter, 'is_site': is_site}, 'rating': rating})
 
 
 def registration(request):
