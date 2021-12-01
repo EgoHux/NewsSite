655f270cfef628e1d85aec16150d852be42ed6fd
Dmitry Aratin <dima.aratin0@gmail.com>
2021-12-01 21:39:44 +0300

    Добавил профили и возможность открытия чужого профиля

--- TopNews/main/urls.py
+++ TopNews/main/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.contrib.auth import views as authViews
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
@@ -9,10 +10,12 @@ urlpatterns = [
     path('about', views.about, name="about"),
     path('create', views.create, name='create'),
     path('accounts/login', views.login, name='login'),
+    path('exit/', authViews.LogoutView.as_view(next_page='news'), name='exit'),
     path('news', views.list_news, name='news'),
     path('news/top', views.top_news, name='top_news'),
     path('news/<int:id>', views.news_frame, name='news_frame'),
     path('accounts/registration/', views.registration, name='registration'),
+    path('profiles/<int:id>', views.profiles, name='profiles')
 ]
 
 # включаем возможность обработки картинок
--- TopNews/main/views.py
+++ TopNews/main/views.py
@@ -8,51 +8,34 @@ def index(request):
     posts = Posts.objects.all()
     return render(request, 'main/index.html', {'title': 'Main Page', 'posts': posts})
 
-
 def about(request):
     return render(request, 'main/about.html')
-
 
 def create(request):
     return render(request, 'main/create.html')
 
-
 def login(request):
-    print("start")
     if request.method == "POST":
         email = request.POST.get('email')
         password = request.POST.get('password')
+
         user = auth.authenticate(username = email, password = password)
-        print(user)
         if user is not None:
             auth.login(request, user)
             return HttpResponseRedirect("/news")
-    # validate = True
-    # if request.method == "POST":
-    #     login = request.POST.get('login')
-    #     password = request.POST.get('password')
-    #     # пользователь не зарегистрирован
-    #     # Логин и пароль не валидны
-    #     count_user = Users.objects.filter(login = login, password = password)
-    #     if not (Users.objects.filter(login = login, password = password)):
-    #         validate = False
-    #     if (validate == True):
-    #         user = Users.objects.filter(login=login, password=password).values()[0]
-    #         response = HttpResponseRedirect('news')
-    #         response.set_cookie('user', user)
-    #         return response
 
     return render(request, 'registration/login.html')
 
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect("/news")
 
 def list_news(request):
     posts = Posts.objects.all()
     return render(request, 'main/news.html', {'title': 'Main Page', 'posts': posts})
 
-
 def top_news(request):
     return render(request, 'main/top_news.html')
-
 
 def news_frame(request, id):
     post = Posts.objects.get(id=id)
@@ -72,7 +55,6 @@ def news_frame(request, id):
 
     return render(request, 'main/news_frame.html', {'title': 'title...', 'post': post, 'reporter_info': {'reporter': reporter, 'is_site': is_site}, 'rating': rating, 'comments': comments})
 
-
 def registration(request):
     if (request.method == 'POST'):
         email = request.POST.get('login')
@@ -83,5 +65,15 @@ def registration(request):
 
         user = Users.objects.create_user(name=username, password=password, username=email, age=age, image=image)
         user.save()
-        print(login)
+
+        auth.login(request, user)
+        return HttpResponseRedirect("/news")
+
     return render(request, 'registration/registration.html')
+
+def profiles(request, id):
+    if request.method == 'GET':
+        user = Users.objects.get(id=id)
+        return render(request, 'main/profiles.html', {'usr': user})
+    if request.method == 'POST':
+        return "true"
